66b1f324fc5dd97e772af010a20ebb069b5ff899
你如此美妙  <gnrcmm@hotmail.com>
2020-11-23 20:46:00 +0800

    '完成侧边栏的布局以及折叠与展开和菜单总体的折叠和展开'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,36 +2,43 @@
     <el-container class="home-container">
         <!--头部区域-->
         <el-header>
-           <div>
-               <img src="../assets/heima.png" alt="">
-               <span>电商平台管理系统</span>
-           </div>
+            <div>
+                <img src="../assets/heima.png" alt="">
+                <span>电商平台管理系统</span>
+            </div>
             <el-button type="info" @click="logout">退出</el-button>
         </el-header>
         <!--页面主体区域-->
         <el-container>
             <!--侧边栏-->
-            <el-aside width="200px">
+            <el-aside :width="isCollapse?'64px':'200px'">
+                <!--展开或折叠所有菜单-->
+                <div class="toggle-button" @click="toggleCollapse">|||</div>
                 <el-menu
                         background-color="#333744"
                         text-color="#fff"
-                        active-text-color="#ffd04b">
+                        active-text-color="#409eff"
+                        :unique-opened="true"
+                        :collapse="isCollapse"
+                        :collapse-transition="false"><!-- :unique-opened='true' 设置只允许展开一个菜单-,collapse设置菜单是否折叠-->
                     <!--一级菜单-->
-                    <el-submenu index="1">
+                    <!-- element-ui 通过index属性来确定操作那个菜单 -->
+                    <el-submenu :index="index.toString()" v-for="(item,index) in menuList" :key="item.id">
                         <!--一级菜单的模板区-->
                         <template slot="title">
                             <!--一级菜单的图标-->
-                            <i class="el-icon-location"></i>
+                            <i :class="iconObj[item.id]"></i>
                             <!--一级菜单的文本内容-->
-                            <span>导航一</span>
+                            <span>{{item.authName}}</span>
                         </template>
                         <!--一级菜单的子菜单，二级菜单-->
-                        <el-menu-item index="1-1">
+                        <!-- element-ui 通过index属性来确定操作那个菜单 -->
+                        <el-menu-item :index="index+'-'+subIndex" v-for="(subItem,subIndex) in item.children" :key="subItem.id">
                             <template slot="title">
                                 <!--二级菜单的图标-->
-                                <i class="el-icon-location"></i>
+                                <i class="el-icon-menu"></i>
                                 <!--二级菜单的文本内容-->
-                                <span>导航2</span>
+                                <span>{{subItem.authName}}</span>
                             </template>
                         </el-menu-item>
 
@@ -47,11 +54,39 @@
 
 <script>
     export default {
+        data() {
+            return {
+                menuList: [],
+                iconObj:{
+                    '125':'iconfont icon-user',
+                    '103':'iconfont icon-tijikongjian',
+                    '101':'iconfont icon-shangpin',
+                    '102':'iconfont icon-danju',
+                    '145':'iconfont icon-baobiao'
+                },
+                isCollapse:false
+            }
+        },
+        created() {
+            /*再模板渲染之前获得数据,以便进行渲染*/
+            this.getMenuList();
+        },
         methods: {
             logout() {
                 /* 删除 token，重定向到登录页面*/
                 window.sessionStorage.clear();
                 this.$router.push("/login");
+            },
+            /*获取所有的菜单*/
+            async getMenuList() {
+                /*  别忘使用 await关键字, 在获得后台的数据后再往下执行*/
+                const {data: res} =  await  this.$http.get("menus")
+                if (res.meta.status !== 200) this.$message.error(res.meta.message);
+                this.menuList = res.data;
+            },
+            /* 点击按钮切换菜单的折叠和展开*/
+            toggleCollapse(){
+                this.isCollapse=!this.isCollapse;
             }
         }
 
@@ -65,29 +100,47 @@
 
     /*在element-ui中 el-xxx 本身就是一个类名*/
     .el-header {
-        display:flex;
+        display: flex;
         justify-content: space-between;
-        padding-left:0;
+        padding-left: 0;
         /*flex布局中单行垂直居中*/
         align-items: center;
         background-color: #373D41;
-        color:#fff;
-        font-size:20px;
+        color: #fff;
+        font-size: 20px;
         /* 设置logo的样式*/
-        > div{
-            display:flex;
+
+        > div {
+            display: flex;
             align-items: center;
-            span{
-                margin-left:15px;
+
+            span {
+                margin-left: 15px;
             }
         }
     }
 
     .el-aside {
         background-color: #333744;
+        .el-menu{
+            border-right:none;
+        }
     }
 
     .el-main {
         background-color: #EAEDF1;
     }
+    .iconfont{
+        margin-right:10px;
+    }
+    .toggle-button{
+        background-color:#4A5064;
+        font-size:10px;
+        text-align:center;
+        line-height:24px;
+        color:#fff;
+        letter-spacing:3px;
+        cursor:point;
+    }
 </style>
+er
